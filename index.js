f5d8181e5d70d102f98a145e99704fadf67bc756
Manya <mayavi1319@gmail.com>
2025-03-03 23:14:48 +0530

    Deploying API #1

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const { resolve } = require('path');
+require ('dotenv').config();
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
@@ -13,3 +20,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+module.exports = config;
+console.log(config);
